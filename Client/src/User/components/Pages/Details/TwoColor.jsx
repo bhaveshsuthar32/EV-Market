c2b7ecc140594678e363d0c4dc46d9e33831ad93
unknown <bagrabhavesh426@gmail.com>
2024-07-09 17:33:28 +0530

    edit page

--- Client/src/User/components/Pages/Details/TwoColor.jsx
+++ Client/src/User/components/Pages/Details/TwoColor.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function TwoColor() {
   return (
-    <div>
-      <h1>Hello Two Color</h1>
-    </div>
-  )
+    <>
+      <div className="">
+        <div className="container md:px-[120px]">
+          <div className="container ">
+            <h1 className="text-[30px] mb-2">OLA S1 Pro Colours</h1>
+            <p className="border-b-[1px] py-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
+              veritatis consectetur voluptates in quae eum? Accusantium dolor
+              aliquam placeat eum laboriosam magnam provident natus eligendi
+              facere? Quo velit sint exercitationem.
+            </p>
+          </div>
+          <div className="container mt-2 flex ">
+            <div className="border-b-[1px] w-full">
+              <Link className="font-semibold" to={"#"}>
+                Color
+              </Link>
+              <Link className="px-6 font-semibold" to={"#"}>
+                Image
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
