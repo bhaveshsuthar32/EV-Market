35d2e80dd335d60b134e948155942fb88bf8031b
Bhavesh Suthar <127782206+bhaveshsuthar32@users.noreply.github.com>
2024-07-12 17:44:39 +0530

    Update ThreeColor.jsx

--- Client/src/User/components/Pages/Details/ThreeColor.jsx
+++ Client/src/User/components/Pages/Details/ThreeColor.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 
 export default function ThreeColor() {
   return (
-    <div>
-      <h1>Hello Three Color</h1>
-      <h1>add color</h1>
-    </div>
+<>
+ <div className="">
+        <div className="container md:px-[120px]">
+          <div className="container ">
+            <h1 className="text-[30px] mb-2">OLA S1 Pro Colours</h1>
+            <p className=" py-3">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
+              veritatis consectetur voluptates in quae eum? Accusantium dolor
+              aliquam placeat eum laboriosam magnam provident natus eligendi
+              facere? Quo velit sint exercitationem.
+            </p>
+          </div>
+          <div className="container mt-2 flex ">
+            <div className="border-b-[1px] w-full">
+              <Link className="font-semibold" to={"#"}>
+                Color
+              </Link>
+              <Link className="px-6 font-semibold" to={"#"}>
+                Image
+              </Link>
+            </div>
+          </div> 
+        </div>
+</div>
+</>
   )
 }
