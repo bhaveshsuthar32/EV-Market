3302c2c4c2801db5e3199e36b4ab5905db4a4629
Bhavesh Suthar <127782206+bhaveshsuthar32@users.noreply.github.com>
2024-07-16 22:57:18 +0530

    Update index.jsx

--- Client/src/User/components/Home/index.jsx
+++ Client/src/User/components/Home/index.jsx
@@ -444,7 +444,7 @@ export function Home() {
         {/* Category */}
         <Footer />
 
-        <h1 className="text-[100px]">Hello world</h1>
+        <h1 className="text-[100px]">add new section</h1>
       </div>
     </>
   );
