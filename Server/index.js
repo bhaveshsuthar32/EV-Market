6625d93d5f1ccecd9ca7b0a56b17bb24af5c1338
bhaveshsuthar32 <bagrabhavesh426@gmail.com>
2024-08-01 17:32:17 +0530

    add file

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,11 @@
-console.log("backend main page")
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.get("/",(req,res)=>{
+    res.send("welcome home page")
+})
+
+app.listen(port, 'localhost' , () => {
+    console.log(`server started on http://localhost:${port}`);
+})
