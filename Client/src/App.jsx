8ed232f0e6853e9c3881646704f214ff591e8793
bhaveshsuthar32 <bagrabhavesh426@gmail.com>
2024-09-06 16:44:39 +0530

    server upload

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import "@assets/bootstrap-datetimepicker.min.css";
-import "@assets/bootstrap.min.css";
-import "@assets/feather.css";
-import "@assets/owl.theme.default.min.css";
-import "@assets/slick.css";
-import "@assets/style.css";
-// js
-import "@assets_js/jquery-3.6.3.min.js";
-import "@assets_js/bootstrap.bundle.min.js";
-import "@assets_js/backToTop.js";
-import "@assets_js/feather.min.js";
-import "@assets_js/owl.carousel.min.js";
-import "@assets_js/script.js";
+// import "@assets/bootstrap-datetimepicker.min.css";
+// import "@assets/bootstrap.min.css";
+// import "@assets/feather.css";
+// import "@assets/owl.theme.default.min.css";
+// import "@assets/slick.css";
+// import "@assets/style.css";
+// // js
+// import "@assets_js/jquery-3.6.3.min.js";
+// import "@assets_js/bootstrap.bundle.min.js";
+// import "@assets_js/backToTop.js";
+// import "@assets_js/feather.min.js";
+// import "@assets_js/owl.carousel.min.js";
+// import "@assets_js/script.js";
 
 import { Home } from "@components/Home";
 import { Error404 } from "@components/Pages/404Error";
