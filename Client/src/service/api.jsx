109bdb8a538214626d2c890e6c3a3506be561abc
bhaveshsuthar32 <bagrabhavesh426@gmail.com>
2024-09-07 10:32:18 +0530

    client upload

--- Client/src/service/api.jsx
+++ Client/src/service/api.jsx
@@ -1,22 +1,22 @@
 import axios from "axios";
 
-const URL = "https://ev-market-server.vercel.app"
+// const URL = "https://ev-market-server.vercel.app"
 
-export const signData = async(data)=>{
-    try {
-        return await axios.post(`${URL}/Admin/sign`, data)
-    } catch (error) {
-        console.log("Error :- " , error);
-    }
-}
+// export const signData = async(data)=>{
+//     try {
+//         return await axios.post(`${URL}/Admin/sign`, data)
+//     } catch (error) {
+//         console.log("Error :- " , error);
+//     }
+// }
 
-export const loginData = async (data) => {
-    try {
-      const response = await axios.post(`${URL}/Admin/login`, data);
-      return response; 
-    } catch (error) {
-      console.error("Error:", error.response ? error.response.data : error.message);
-      throw error; 
-    }
-  };
+// export const loginData = async (data) => {
+//     try {
+//       const response = await axios.post(`${URL}/Admin/login`, data);
+//       return response; 
+//     } catch (error) {
+//       console.error("Error:", error.response ? error.response.data : error.message);
+//       throw error; 
+//     }
+//   };
 
