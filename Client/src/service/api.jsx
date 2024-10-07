cc5998eb3ae6d08b1b2f2cbfc0d7d013345f463a
Bhavesh Suthar <127782206+bhaveshsuthar32@users.noreply.github.com>
2024-10-07 23:42:47 +0530

    Update api.jsx

--- Client/src/service/api.jsx
+++ Client/src/service/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
- const URL = "https://ev-market-server.vercel.app"
-// const URL = "http://localhost:4000"
+ // const URL = "https://ev-market-server.vercel.app"
+const URL = "http://localhost:4000"
 
 export const signData = async(data)=>{
     try {
