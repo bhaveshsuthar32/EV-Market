c60e85705a0dbe46d82ae35b8e9d5f5249600a3c
bhavesh suthar <bagrabhavesh426@gmail.com>
2024-11-15 22:33:33 +0530

    feat : create twoWheeler model

---
+++ Server/model/twoWheeler.js
@@ -0,0 +1,68 @@
+const mongoose  = require("mongoose");
+
+const twoSchema = new mongoose.Schema({
+    type : {
+        type : String,
+        required : false,
+    },
+    brand : {
+        type : String,
+        required : false,
+    },
+    upcomming_and_used : {
+        type : String,
+        required : false,
+    },
+    vehicle_name : {
+        type : String,
+        required : false,
+    },
+    speed : {
+        type : String,
+        required : false,
+    },
+    range : {
+        type : String,
+        required : false,
+    },
+    motor_power : {
+        type : String,
+        required : false,
+    },
+    battery : {
+        type : String,
+        required : false,
+    },
+    charging_time : {
+        type : String,
+        required : false,
+    },
+    battery_charger : {
+        type : String,
+        required : false,
+    },
+    showroom_price : {
+        type : String,
+        required : false,
+    },
+    color : {
+        type : String,
+        required : false,
+    },
+    img1 : {
+        type : String,
+        required : false,
+    },
+    img2 : {
+        type : String,
+        required : false,
+    },
+    img3 : {
+        type : String,
+        required : false,
+    },
+},{ timestamps: true });
+
+const twoWheeler = mongoose.model("twoWheeler", twoSchema);
+
+module.exports = twoWheeler ;
