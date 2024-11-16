e5e8304b79eb49a6c4fb0ce65824c60056da35f5
bhavesh suthar <bagrabhavesh426@gmail.com>
2024-11-16 16:45:24 +0530

    refactor : add getTwoWheelr routes

--- Server/Router/router.js
+++ Server/Router/router.js
@@ -8,15 +8,15 @@ const authenticateToken = require('../middleware/authMiddleware');
 const { addTwoDetails, getTwoDetails } = require('../controllers/wheelerController');
 
 
-router.post("/Admin/sign", signAdmin)
-router.post("/Admin/login", loginAdmin)
-router.get("/Admin/getAdmin", authenticateToken, getAdmin)
-router.post("/Admin/addTwoWheeler",upload.fields([
+router.post("/admin/sign", signAdmin)
+router.post("/admin/login", loginAdmin)
+router.get("/admin/getAdmin", authenticateToken, getAdmin)
+router.post("/admin/addTwoWheeler",upload.fields([
     { name: 'img1', maxCount: 1 },
     { name: 'img2', maxCount: 1 },
     { name: 'img3', maxCount: 1 },
   ]), addTwoDetails)
-router.get("/Admin/getTwoWheeler",  getTwoDetails)
+router.get("/admin/getTwoWheeler",  getTwoDetails)
 
 // export default router ;
 module.exports = router;
