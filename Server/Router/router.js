78848add0efa42b002bc62c46938119444e469f5
bhavesh suthar <bagrabhavesh426@gmail.com>
2024-11-16 14:39:06 +0530

    refactor : add upload middleware for image

--- Server/Router/router.js
+++ Server/Router/router.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const upload = require('../middleware/upload');
 
 
-const { signAdmin, loginAdmin, getAdmin } = require('../controller/adminController');
+const { signAdmin, loginAdmin, getAdmin } = require('../controllers/adminController');
 const authenticateToken = require('../middleware/authMiddleware');
-const { addTwoDetails, getTwoDetails } = require('../controller/wheelerController');
+const { addTwoDetails, getTwoDetails } = require('../controllers/wheelerController');
 
 
 router.post("/Admin/sign", signAdmin)
 router.post("/Admin/login", loginAdmin)
 router.get("/Admin/getAdmin", authenticateToken, getAdmin)
-router.post("/Admin/addTwoWheeler", addTwoDetails)
-router.get("/Admin/getTwoWheeler", getTwoDetails)
+router.post("/Admin/addTwoWheeler",upload.fields([
+    { name: 'img1', maxCount: 1 },
+    { name: 'img2', maxCount: 1 },
+    { name: 'img3', maxCount: 1 },
+  ]), addTwoDetails)
+router.get("/Admin/getTwoWheeler",  getTwoDetails)
 
 // export default router ;
 module.exports = router;
