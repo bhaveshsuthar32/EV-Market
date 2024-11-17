840d02c6f465a396eed580d33e528ba7392e0664
bhavesh suthar <bagrabhavesh426@gmail.com>
2024-11-17 19:32:08 +0530

    feat : create four wheeler page

--- Client/src/pages/admin/pages/four-wheeler/index.jsx
+++ Client/src/pages/admin/pages/four-wheeler/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function fourWheeler() {
+export default function FourWheeler() {
   return (
     <div>
-      
+      <h1>Four Wheeler</h1>
     </div>
   )
 }
