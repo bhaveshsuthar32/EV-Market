855ea3d143ec0974302e9f2451a6fe1631ebe955
bhavesh suthar <bagrabhavesh426@gmail.com>
2024-11-23 23:24:09 +0530

    refactor : modify page for add delete feature

--- Client/src/pages/admin/pages/two-wheeler/index.jsx
+++ Client/src/pages/admin/pages/two-wheeler/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { getTwoData } from '../../../../api'
+import { deleteTwoWheeler, getTwoData } from '../../../../api'
 import { Link } from "react-router-dom";
 
 export default function TwoWheeler() {
@@ -13,6 +13,19 @@ export default function TwoWheeler() {
       console.log("Error :- ", error);
     }
   }
+
+  const handleDeleteClick = async (id) => {
+    if (window.confirm("Are you sure you want to delete this two-wheeler?")) {
+      try {
+        await deleteTwoWheeler(id);
+        // Update state after deletion
+        getTwoWheelerData();
+      } catch (error) {
+        console.error("Error deleting data:", error);
+      }
+    }
+  };
+
 
   useEffect(() => {
     getTwoWheelerData();
@@ -79,7 +92,7 @@ export default function TwoWheeler() {
                         <tr>
                           <td>
                             <button
-                            // onClick={() => handleDeleteClick(twoDataInfo._id)}
+                            onClick={() => handleDeleteClick(twoDataInfo._id)}
                             >
                               <svg
                                 xmlns="http://www.w3.org/2000/svg"
@@ -94,6 +107,10 @@ export default function TwoWheeler() {
                                   d="M 14.984375 2.4863281 A 1.0001 1.0001 0 0 0 14 3.5 L 14 4 L 8.5 4 A 1.0001 1.0001 0 0 0 7.4863281 5 L 6 5 A 1.0001 1.0001 0 1 0 6 7 L 24 7 A 1.0001 1.0001 0 1 0 24 5 L 22.513672 5 A 1.0001 1.0001 0 0 0 21.5 4 L 16 4 L 16 3.5 A 1.0001 1.0001 0 0 0 14.984375 2.4863281 z M 6 9 L 7.7929688 24.234375 C 7.9109687 25.241375 8.7633438 26 9.7773438 26 L 20.222656 26 C 21.236656 26 22.088031 25.241375 22.207031 24.234375 L 24 9 L 6 9 z"
                                 ></path>
                               </svg>
+                            </button>
+                            <button className='border-2'>
+                              <Link to={`/admin/twoWheeler-view/${twoDataInfo._id}`}>view
+                              </Link>
                             </button>
                             <button
                             // onClick={() => handleEditClick(twoDataInfo._id)}
