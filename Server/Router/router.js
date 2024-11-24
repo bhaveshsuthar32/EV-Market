7d439405b14835a5c13b1634d1b10df57eb773a5
bhavesh suthar <bagrabhavesh426@gmail.com>
2024-11-24 21:48:58 +0530

    feat : add new route

--- Server/Router/router.js
+++ Server/Router/router.js
@@ -10,6 +10,7 @@ const upload = require('../middleware/upload');
 const adminApi = require('../controllers/adminController');
 const authenticateToken = require('../middleware/authMiddleware');
 const twoWheeler = require('../controllers/wheelerController');
+const threeWheeler = require('../controllers/wheelerController');
 const otherData = require("../controllers/otherDetailsController")
 
 router.post("/admin/sign", adminApi.signAdmin)
@@ -39,6 +40,22 @@ router.get("/getBikeUsed", twoWheeler.getBikeUsed);
 router.get("/getScooterData", twoWheeler.getScooterData);
 router.get("/getScooterUpcoming", twoWheeler.getScooterUpcoming);
 router.get("/getScooterUsed", twoWheeler.getScooterUsed);
+
+
+
+router.post("/admin/addThreeWheeler", upload.fields([
+  { name: 'img1', maxCount: 1 },
+  { name: 'img2', maxCount: 1 },
+  { name: 'img3', maxCount: 1 },
+]),threeWheeler.addThreeData);
+router.get("/admin/getThreeWheeler", threeWheeler.getThreeData)
+router.get("/admin/getThreeById/:id", threeWheeler.getThreeDataById)
+router.delete("/admin/deleteThreeWheeler/:id", threeWheeler.deleteThreeDetails)
+
+
+router.get("/getRickshawData", threeWheeler.getRickshawData);
+router.get("/getRickshawUpcoming", threeWheeler.getRickshawUpcoming);
+router.get("/getRickshawUsed", threeWheeler.getRickshawUsed);
 
 // export default router ;
 module.exports = router;
