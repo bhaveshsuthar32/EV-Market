2e703f578da8fc7b8fca9ec90e8cd870a3c30507
bhavesh suthar <bagrabhavesh426@gmail.com>
2024-11-28 17:45:53 +0530

    refactor: update page design and optimize layout for mobile responsiveness

--- Client/src/pages/user/pages/two-wheeler/scooter.jsx
+++ Client/src/pages/user/pages/two-wheeler/scooter.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navbar } from "../../components/Navbar";
 import { Footer } from "../../components/Footer";
+import { getScooterData } from "../../../../api";
+import { Link } from "react-router-dom";
 
 export function ScooterData() {
+
+  const [scooter, setScooter] = useState([]);
+
+  const scooterData = async () => {
+    const response = await getScooterData()
+    setScooter(response.data);
+  }
+
+  useEffect(() => {
+    scooterData();
+  }, [])
+
   return (
     <>
       <div className="main-wrapper">
 
-      <Navbar />
+        <Navbar />
 
         <div className="list-content  mt-[60px] mt-lg-[60px]">
           <div className="container">
@@ -15,97 +29,131 @@ export function ScooterData() {
               <div className="col-lg-12">
                 <div className=" listgrid-sidebar">
                   <div className="count-search mb-[20px] ">
-                    <h4>
-                      E-Scooter for Sale{" "}
-                      <span style={{ fontSize: "14px" }}>(Showing 12)</span>
+                    <h4 className="text-[20px] font-semibold">
+                      E-Scooter for Sale
+                      <span className="text-[16px]"> (Showing {scooter.length})</span>
                     </h4>
                   </div>
                   <div className="grid-view listgrid-sidebar row">
-
-                    <div className="col-12 col-lg-3 col-md-4 col-sm-12 ">
-                      <div className="card">
-                        <div className="blog-widget">
-                          <div className="blog-img text-center p-2">
-                            <a href="">
-                              <img
-                                src={""}
-                                className="img-fluid"
-                                style={{ height: "170px", width: "150px" }}
-                                alt="blog-img"
-                              />
-                            </a>
-                          </div>
-                          <div className="bloglist-content">
-                            <div className="card-body p-3">
-                              <div className="amount-details mb-2 ps-2 fw-bold text-dark">
-                                <span>Abcd</span>
-                                <div className="ratings">
-                                  <span>New</span>
+                    {scooter.map((scooterInfo) => (
+                      <div className="col-12 col-lg-3 col-md-4 col-sm-12 " key={scooterInfo._id}>
+                        <div className="card">
+                          <div className="blog-widget">
+                            <div className=" text-center p-2">
+                              <Link to={`/two-details/${scooterInfo._id}`}>
+                                <img
+                                  src={scooterInfo.img1}
+                                  className="img-fluid h-[170px] object-cover transform-none hover:transform-none px-1"
+                                  // style={{ height: "170px", width: "150px" }}
+                                  alt={scooterInfo.vehicle_name}
+                                />
+                              </Link>
+                            </div>
+                            <div className="bloglist-content">
+                              <div className="card-body p-3">
+                                <div className="amount-details mb-2 ps-2 fw-bold text-dark">
+                                  <span>{scooterInfo.vehicle_name}</span>
+                                  <div className="ratings">
+                                    <span>New</span>
+                                  </div>
                                 </div>
-                              </div>
-                              <div className="blogfeaturelink m-0">
-                                <div className="blog-features ps-2 fw-bold mb-2">
-                                  <p>₹ 101</p>
+                                <div className="blogfeaturelink m-0">
+                                  <div className="blog-features ps-2 fw-bold mb-2">
+                                    <p>₹ {scooterInfo.showroom_price}</p>
+                                  </div>
                                 </div>
-                              </div>
-                              <div className="blog-location-details mt-1 grid grid-cols-4 text-center text-xs text-black mt-[-20px]">
-                                <div>
-                                  <img
-                                    src="/assets_1/My_img/logo-1.png"
-                                    alt=""
-                                    className="w-8"
-                                  />
-                                  <p>Motor Type</p>
+                                <div className="blog-location-details grid grid-cols-3 text-center text-xs text-black ">
+                                  <div>
+                                    <img
+                                      src="/assets_1/My_img/logo-1.png"
+                                      alt=""
+                                      className="w-8"
+                                    />
+                                    <p>Motor Power</p>
+                                  </div>
+                                  <div>
+                                    <img
+                                      src="/assets_1/My_img/logo-2.png"
+                                      alt=""
+                                      className="w-8"
+                                    />
+                                    <p>Range</p>
+                                  </div>
+                                  <div>
+                                    <img
+                                      src="/assets_1/My_img/logo-3.png"
+                                      alt=""
+                                      className="w-8"
+                                    />
+                                    <p>Battery</p>
+                                  </div>
+                                 
                                 </div>
-                                <div>
-                                  <img
-                                    src="/assets_1/My_img/logo-2.png"
-                                    alt=""
-                                    className="w-8"
-                                  />
-                                  <p>Range</p>
+                                <div className="blog-location-details text-dark grid grid-cols-3 text-center text-xs mt-[-20px]">
+                                  <div>{scooterInfo.motor_power}</div>
+                                  <div>{scooterInfo.range}</div>
+                                  <div>{scooterInfo.battery}</div>
+                                  
                                 </div>
-                                <div>
-                                  <img
-                                    src="/assets_1/My_img/logo-3.png"
-                                    alt=""
-                                    className="w-8"
-                                  />
-                                  <p>ChargeTime</p>
+                                <div className="blog-location-details mt-1 grid grid-cols-3 text-center text-xs text-black mt-[-20px]">
+                                  <div>
+                                    <img
+                                      src="/assets_1/My_img/logo-1.png"
+                                      alt=""
+                                      className="w-8"
+                                    />
+                                    <p>Charging Time</p>
+                                  </div>
+                                  <div>
+                                    <img
+                                      src="/assets_1/My_img/logo-2.png"
+                                      alt=""
+                                      className="w-8"
+                                    />
+                                    <p>Color</p>
+                                  </div>
+                                  <div>
+                                    <img
+                                      src="/assets_1/My_img/logo-3.png"
+                                      alt=""
+                                      className="w-8"
+                                    />
+                                    <p>Speed</p>
+                                  </div>
+                                  <div>
+                                    <img
+                                      src="/assets_1/My_img/logo-4.png"
+                                      alt=""
+                                      className="w-8"
+                                    />
+                                  
+                                  </div>
                                 </div>
-                                <div>
-                                  <img
-                                    src="/assets_1/My_img/logo-4.png"
-                                    alt=""
-                                    className="w-8"
-                                  />
-                                  <p>Transmission</p>
+                                <div className="blog-location-details text-dark grid grid-cols-3 text-center text-xs mt-[-20px]">
+                                  <div>{scooterInfo.charging_time}</div>
+                                  <div>{scooterInfo.color}</div>
+                                  <div>{scooterInfo.speed}</div>
                                 </div>
-                              </div>
-                              <div className="blog-location-details text-dark grid grid-cols-4 text-center text-xs mt-[-20px]">
-                                <div>abcd1</div>
-                                <div>abcd3</div>
-                                <div>abcd5</div>
-                                <div>Automatic Transmission</div>
-                              </div>
-                              <div className="amount-details">
-                                <div className="amount">
-                                  <a className="validrate" href="">
-                                    <button className="bg-success text-white text-xs p-1 rounded">
-                                      View data
-                                    </button>
-                                  </a>
-                                </div>
-                                <div className="ratings mt-2">
-                                  <input type="checkbox" /> Add To Compare
+                                <div className="amount-details">
+                                  <div className="amount">
+                                    <Link className="validrate" to={`/two-details/${scooterInfo._id}`}>
+                                      <button className="bg-success text-white text-xs p-1 rounded">
+                                        View data
+                                      </button>
+                                    </Link>
+                                  </div>
+                                  <div className="ratings mt-2">
+                                    <input type="checkbox" /> Add To Compare
+                                  </div>
                                 </div>
                               </div>
                             </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                    
+                    ))}
+
+
                   </div>
                 </div>
               </div>
